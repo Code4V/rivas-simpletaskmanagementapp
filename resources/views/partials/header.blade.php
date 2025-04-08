b5f7ba330084f92f41c499c549c68244a5c0e79b
Jeanne Francis Rivas <vashypies@gmail.com>
2025-04-08 13:57:29 +0800

    initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">Document
+  @vite('resources/css/app.css')
+  <title>Simple Task Management</title>
+</head>
+<body>
